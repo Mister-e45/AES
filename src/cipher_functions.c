1af929dee2dfdf7c98e9f6d6554fdfb1c16f5f93
Amankwah Emmanuel <emmanuel.amankwah@etudiant.univ-rennes1.fr>
2024-10-13 19:19:34 +0200

    commit 2

---
+++ src/cipher_functions.c
@@ -0,0 +1,378 @@
+#include "cipher_functions.h"
+
+#define Nb 4
+
+static  Byte abyte0=0x0;// used in shiftRows
+static  Byte abyte1=0x0;// used in shiftRows
+
+static const Byte ax[4]={0x2,0x1,0x1,0x3};
+static const Byte  axinv[4]={0xe,0x9,0xd,0xb};
+
+static Byte temp0=0x0; //used in RotWord
+static Byte temp1=0x0; //used in RotWord
+static Byte temp2=0x0; //used in RotWord
+static Byte temp3=0x0; //used in RotWord
+
+Byte key[32]={};
+static int keySize=0; //in bits
+
+static int keyIsLoaded=0; 
+static int Nr=0; // is properly set by takeKey function
+
+Byte roundKeys[240]={}; // the size fits all three key length possibilities at most 15 rounds with 14 16-byte key generated
+
+//state[j][i]= i-th element from the top of the j-th column
+static Byte state[4][4]={ {0x0,0x0,0x0,0x0}, //represents column 0
+                         {0x0,0x0,0x0,0x0}, //represents column 1
+                         {0x0,0x0,0x0,0x0}, //represents column 2
+                         {0x0,0x0,0x0,0x0}  //rerpesents column 3
+                        };
+
+static const Byte SBox[256]={ 
+    // 0     1    2      3     4    5     6     7      8    9     A      B    C     D     E     F
+    0x63, 0x7c, 0x77, 0x7b, 0xf2, 0x6b, 0x6f, 0xc5, 0x30, 0x01, 0x67, 0x2b, 0xfe, 0xd7, 0xab, 0x76,  // 0
+    0xca, 0x82, 0xc9, 0x7d, 0xfa, 0x59, 0x47, 0xf0, 0xad, 0xd4, 0xa2, 0xaf, 0x9c, 0xa4, 0x72, 0xc0,  // 1
+    0xb7, 0xfd, 0x93, 0x26, 0x36, 0x3f, 0xf7, 0xcc, 0x34, 0xa5, 0xe5, 0xf1, 0x71, 0xd8, 0x31, 0x15,  // 2
+    0x04, 0xc7, 0x23, 0xc3, 0x18, 0x96, 0x05, 0x9a, 0x07, 0x12, 0x80, 0xe2, 0xeb, 0x27, 0xb2, 0x75,  // 3
+    0x09, 0x83, 0x2c, 0x1a, 0x1b, 0x6e, 0x5a, 0xa0, 0x52, 0x3b, 0xd6, 0xb3, 0x29, 0xe3, 0x2f, 0x84,  // 4
+    0x53, 0xd1, 0x00, 0xed, 0x20, 0xfc, 0xb1, 0x5b, 0x6a, 0xcb, 0xbe, 0x39, 0x4a, 0x4c, 0x58, 0xcf,  // 5
+    0xd0, 0xef, 0xaa, 0xfb, 0x43, 0x4d, 0x33, 0x85, 0x45, 0xf9, 0x02, 0x7f, 0x50, 0x3c, 0x9f, 0xa8,  // 6
+    0x51, 0xa3, 0x40, 0x8f, 0x92, 0x9d, 0x38, 0xf5, 0xbc, 0xb6, 0xda, 0x21, 0x10, 0xff, 0xf3, 0xd2,  // 7
+    0xcd, 0x0c, 0x13, 0xec, 0x5f, 0x97, 0x44, 0x17, 0xc4, 0xa7, 0x7e, 0x3d, 0x64, 0x5d, 0x19, 0x73,  // 8
+    0x60, 0x81, 0x4f, 0xdc, 0x22, 0x2a, 0x90, 0x88, 0x46, 0xee, 0xb8, 0x14, 0xde, 0x5e, 0x0b, 0xdb,  // 9
+    0xe0, 0x32, 0x3a, 0x0a, 0x49, 0x06, 0x24, 0x5c, 0xc2, 0xd3, 0xac, 0x62, 0x91, 0x95, 0xe4, 0x79,  // A
+    0xe7, 0xc8, 0x37, 0x6d, 0x8d, 0xd5, 0x4e, 0xa9, 0x6c, 0x56, 0xf4, 0xea, 0x65, 0x7a, 0xae, 0x08,  // B
+    0xba, 0x78, 0x25, 0x2e, 0x1c, 0xa6, 0xb4, 0xc6, 0xe8, 0xdd, 0x74, 0x1f, 0x4b, 0xbd, 0x8b, 0x8a,  // C
+    0x70, 0x3e, 0xb5, 0x66, 0x48, 0x03, 0xf6, 0x0e, 0x61, 0x35, 0x57, 0xb9, 0x86, 0xc1, 0x1d, 0x9e,  // D
+    0xe1, 0xf8, 0x98, 0x11, 0x69, 0xd9, 0x8e, 0x94, 0x9b, 0x1e, 0x87, 0xe9, 0xce, 0x55, 0x28, 0xdf,  // E
+    0x8c, 0xa1, 0x89, 0x0d, 0xbf, 0xe6, 0x42, 0x68, 0x41, 0x99, 0x2d, 0x0f, 0xb0, 0x54, 0xbb, 0x16   // F
+};
+
+static const Byte invSBox[256]={ 0x52,0x9,0x6a,0xd5,0x30,0x36,0xa5,0x38,0xbf,0x40,0xa3,0x9e,0x81,0xf3,0xd7,0xfb,
+                                0x7c,0xe3,0x39,0x82,0x9b,0x2f,0xff,0x87,0x34,0x8e,0x43,0x44,0xc4,0xde,0xe9,0xcb,
+                                0x54,0x7b,0x94,0x32,0xa6,0xc2,0x23,0x3d,0xee,0x4c,0x95,0xb,0x42,0xfa,0xc3,0x4e,
+                                0x8,0x2e,0xa1,0x66,0x28,0xd9,0x24,0xb2,0x76,0x5b,0xa2,0x49,0x6d,0x8b,0xd1,0x25,
+                                0x72,0xf8,0xf6,0x64,0x86,0x68,0x98,0x16,0xd4,0xa4,0x5c,0xcc,0x5d,0x65,0xb6,0x92,
+                                0x6c,0x70,0x48,0x50,0xfd,0xed,0xb9,0xda,0x5e,0x15,0x46,0x57,0xa7,0x8d,0x9d,0x84,
+                                0x90,0xd8,0xab,0x0,0x8c,0xbc,0xd3,0xa,0xf7,0xe4,0x58,0x5,0xb8,0xb3,0x45,0x6,
+                                0xd0,0x2c,0x1e,0x8f,0xca,0x3f,0xf,0x2,0xc1,0xaf,0xbd,0x3,0x1,0x13,0x8a,0x6b,
+                                0x3a,0x91,0x11,0x41,0x4f,0x67,0xdc,0xea,0x97,0xf2,0xcf,0xce,0xf0,0xb4,0xe6,0x73,
+                                0x96,0xac,0x74,0x22,0xe7,0xad,0x35,0x85,0xe2,0xf9,0x37,0xe8,0x1c,0x75,0xdf,0x6e,
+                                0x47,0xf1,0x1a,0x71,0x1d,0x29,0xc5,0x89,0x6f,0xb7,0x62,0xe,0xaa,0x18,0xbe,0x1b,
+                                0xfc,0x56,0x3e,0x4b,0xc6,0xd2,0x79,0x20,0x9a,0xdb,0xc0,0xfe,0x78,0xcd,0x5a,0xf4,
+                                0x1f,0xdd,0xa8,0x33,0x88,0x7,0xc7,0x31,0xb1,0x12,0x10,0x59,0x27,0x80,0xec,0x5f,
+                                0x60,0x51,0x7f,0xa9,0x19,0xb5,0x4a,0xd,0x2d,0xe5,0x7a,0x9f,0x93,0xc9,0x9c,0xef,
+                                0xa0,0xe0,0x3b,0x4d,0xae,0x2a,0xf5,0xb0,0xc8,0xeb,0xbb,0x3c,0x83,0x53,0x99,0x61,
+                                0x17,0x2b,0x4,0x7e,0xba,0x77,0xd6,0x26,0xe1,0x69,0x14,0x63,0x55,0x21,0xc,0x7d };
+
+
+void setState(Byte* s){
+    for (int j =0;j<Nb;++j)
+        for(int i =0;i<Nb;++i){
+            state[j][i]=s[4*j+i];
+        }
+}
+
+void outState(Byte* s){
+    for (int j =0;j<Nb;++j){
+        for(int i =0;i<Nb;++i){
+            s[4*j+i]=state[j][i];
+        }
+    }
+}
+
+void SubBytes(){
+    for(int j=0;j<Nb;++j)
+        for(int i=0;i<Nb;++i){
+            state[j][i]=SBox[ (unsigned int)(state[j][i])];
+        }
+}
+
+void invSubBytes(){
+    for(int j=0;j<Nb;++j)
+        for(int i=0;i<Nb;++i){
+            state[j][i]=invSBox[ (unsigned int)(state[j][i])];
+        }
+}
+
+void ShiftRows(){
+    //remember that state has a special indexation: it's an array of columns not an array of lines!
+
+    //shift row 1:
+    abyte0=state[0][1];
+    state[0][1]=state[1][1];
+
+    abyte1=state[3][1];
+    state[3][1]=abyte0;
+    
+
+    abyte0=state[2][1];
+    state[2][1]=abyte1;
+
+    state[1][1]=abyte0;
+
+    //shift row 2:
+
+    abyte0=state[0][2];
+    state[0][2]=state[2][2];
+
+    state[2][2]=abyte0;
+    
+    abyte0=state[1][2];
+    state[1][2]=state[3][2];
+
+    state[3][2]=abyte0;
+
+    //shift row 3:
+
+    abyte0=state[2][3];
+    state[2][3]=state[1][3];
+
+    abyte1=state[3][3];
+    state[3][3]=abyte0;
+    
+
+    abyte0=state[0][3];
+    state[0][3]=abyte1;
+
+    state[1][3]=abyte0;
+}
+
+void invShiftRows(){
+
+    //shift row 1:
+    abyte0=state[0][1];
+    state[0][1]=state[3][1];
+
+    abyte1=state[1][1];
+    state[1][1]=abyte0;
+    
+
+    abyte0=state[2][1];
+    state[2][1]=abyte1;
+
+    state[3][1]=abyte0;
+    //shift row 2:
+
+    abyte0=state[0][2];
+    state[0][2]=state[2][2];
+
+    state[2][2]=abyte0;
+    
+    abyte0=state[1][2];
+    state[1][2]=state[3][2];
+
+    state[3][2]=abyte0;
+
+    //shift row 3:
+
+    abyte0=state[2][3];
+    state[2][3]=state[3][3];
+
+    abyte1=state[1][3];
+    state[1][3]=abyte0;
+    
+
+    abyte0=state[0][3];
+    state[0][3]=abyte1;
+
+    state[3][3]=abyte0;
+}
+
+void MixColumns(){
+    for(int i=0;i<Nb;++i)
+        multPolynomials(state[i],ax,state[i]);
+}
+
+void invMixColumns(){
+        for(int i=0;i<Nb;++i)
+        multPolynomials(state[i],axinv,state[i]);
+}
+
+void SubWord(Byte* word){
+    
+    for(int i=0;i<Nb;++i)
+        word[i]=SBox[ (unsigned int)(word[i]) ];
+    
+}
+
+void RotWord(Byte* word){
+    temp0=word[0];
+    temp1=word[1];
+    temp2=word[2];
+    temp3=word[3];
+
+    word[0]=temp1;
+    word[1]=temp2;
+    word[2]=temp3;
+    word[3]=temp0;
+}
+
+void AddRoundKey(Byte* rKey){
+    for(int j=0;j<Nb;++j)
+        for(int i=0; i<Nb;++i){
+            state[j][i]=state[j][i] ^ rKey[Nb*j+i];
+        }
+}
+
+void takeKey(Byte* K,int size){ // generates the derived keys
+    keySize=size;
+    if(!keyIsLoaded){
+
+        keyIsLoaded=1;
+        
+        
+
+        int Nk=keySize/32;
+
+        if(keySize==128){
+            Nr=10;
+        }
+            
+        else if(keySize==192){
+            Nr=12;
+        }
+            
+        else if(keySize==256){
+            Nr=14;
+        }
+        else
+            printf("Key size is not supported");
+        
+        Byte powX[17]={};
+        powX[0]=0x0;
+        Byte xPow=0x01;
+
+        for(int i=1;i<=Nr;++i){
+        powX[i]=xPow;
+        xPow=xtime(xPow);
+        }
+
+        Byte temp[4]={};
+        for(int i=0;i<Nk*4;++i){ // the first part is just the key
+            roundKeys[i]=K[i];
+        }
+        
+
+        for(int l=Nk;l<Nb*(Nr+1);++l){ // this loop generates the round keys 
+            temp[0]=roundKeys[4*l-4];
+            temp[1]=roundKeys[4*l-3];
+            temp[2]=roundKeys[4*l-2];
+            temp[3]=roundKeys[4*l-1];
+
+            if(l%Nk==0){
+                RotWord(temp);
+                SubWord(temp);
+                temp[0]= temp[0] ^ powX[l/Nk];
+
+            }
+            else if(Nk>6 && (l%Nk==4) ){
+                SubWord(temp);
+            }
+
+            roundKeys[4*l]=  temp[0] ^ roundKeys[4*(l-Nk)];
+            roundKeys[4*l+1]=temp[1] ^ roundKeys[4*(l-Nk)+1];
+            roundKeys[4*l+2]=temp[2] ^ roundKeys[4*(l-Nk)+2];
+            roundKeys[4*l+3]=temp[3] ^ roundKeys[4*(l-Nk)+3];
+        }
+        
+    }
+
+}
+
+int throwKeys(){ 
+    keyIsLoaded=0;
+    return 1;
+}
+
+void Cipher(Byte* block){
+    setState(block);
+
+    AddRoundKey(roundKeys);
+
+
+    for(int i=1;i<Nr;++i){
+        
+        SubBytes();
+        ShiftRows();
+        MixColumns();
+        AddRoundKey(roundKeys+i*16); //16 because a 128 bits block is made of 16 bytes
+
+    }
+    SubBytes();
+    ShiftRows();
+    AddRoundKey(roundKeys+Nr*16);
+    
+    outState(block);
+    
+}
+
+void deCipher(Byte* block){
+    setState(block);
+
+    AddRoundKey(roundKeys+Nr*16);
+    invShiftRows();
+    invSubBytes();
+    for(int i=1;i<Nr;++i){
+        AddRoundKey(roundKeys+(Nr-i)*16);
+        invMixColumns();
+        invShiftRows();
+        invSubBytes();
+    }
+
+    AddRoundKey(roundKeys);
+
+    outState(block);
+}
+
+int readKey(char* k){//reads the key given as a string of hexadecimal values
+    int i =0; //counts bytes (2 hexs characters)
+    int count=0; // counts the characters in k allows to ignore spaces between hex pairs
+    char* K=k;
+
+    if(k[1]=='x') // if the key is written in 0x... format we ignore the first two characters
+        K=k+2;
+    while(K[count] != 0 && K[count+1] != 0 && i<32){ //32 is the maximum length in bytes of a key
+        while(K[count]==' '){//ignore spaces between hex pairs
+            ++count;
+        }
+        if(K[count]=='\0' || K[count+1]=='\0')//if no character after space exit with 0
+            return 0;
+
+        char bit[3]={K[count],K[count+1],'\0'};
+        key[i]=(Byte)(strtol(bit,NULL,16)); // we convert the hex string to a long int then to a Byte (by truncation)
+        count+=2; //we prepare to read the two next characters
+        ++i; //we increment the byte count
+    }
+        
+    if(i==16 || i==24 || i==32){
+        keySize=i*8;
+    
+        return 1;
+    }
+    
+    else{
+        printf("invalid key size given \n");
+        exit(0);
+    }
+        
+}
+
+void AES(Byte* block,char* k){
+    if(!keyIsLoaded){
+        readKey(k);
+        takeKey(key,keySize);
+    }
+
+    Cipher(block);
+    
+}
+
+void invAES(Byte* block,char* k){
+    if(!keyIsLoaded){
+        readKey(k);
+        takeKey(key,keySize);
+    }
+
+    deCipher(block);
+}
